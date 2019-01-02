aa0fd294d04f082f17c0a3a6e0ec802d738d4401
Joel Cardinal <joelcardinal@gmail.com>
2019-01-01 18:49:18 -0700

    Added train model

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 // these need to be accessed inside more than one function so we'll declare them first
 let container;
 let camera;
+let controls;
 let renderer;
 let scene;
 let mesh;
+
+function initControls() {
+  // remember, this makes the camera orbit the object, not rotate the object
+  controls = new THREE.OrbitControls( camera, container );
+  
+}
 function initCamera(){
   // set up the options for a perspective camera
   const fov = 35; // fov = Field Of View
@@ -27,19 +34,36 @@ function initCamera(){
   // NOTE: CSS coordinate system has y axis poles reversed
 
   // every object is initially created at ( 0, 0, 0 )
-  // we'll move the camera back a bit so that we can view the scene
-  camera.position.set( 0, 0, 10 );
+  // we'll move the camera so not looking straight on 0,0,0 origin
+  camera.position.set( -5, 5, 7 );
 }
 function initLights(){
   // Create a directional light
-  // By default, this target is at (0, 0, 0)
+  // By default, directional light target is at (0, 0, 0)
   // typically, light intensity (here the second parm) should usually be < than 1 
-  const light = new THREE.DirectionalLight( 0xffffff, 3.0 );
+  //const light = new THREE.DirectionalLight( 0xffffff, 3.0 );
   // move the light back and up a bit
-  light.position.set( 0, 3, 3 );
+  //light.position.set( 0, 3, 3 );
   // remember to add the light to the scene
-  scene.add( light );
+  //scene.add( light );
+
+  // NOTE: ambient lighting is used for artistic control rather than strong lighting,
+  // meaning that it is usually set much more dimly than this, typically:
+  // 1. Set the color to a dim grey, somewhere between 0x111111 for a very dim indoor scene and 0xaaaaaa for a bright outdoor scene, while leaving the intensity at 11
+  // 2. leave the color as white and set the intensity very low, somewhere around 0.10.1 to 0.30.3
+  // TODO: how to limit ambient light to only certain objects or otherways to fake ambient occlusion
+  const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
+  scene.add( ambientLight );
+
+  const frontLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  frontLight.position.set( 10, 10, 10 );
+
+  const backLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  backLight.position.set( -10, 10, -10 );
+
+  scene.add( frontLight, backLight );
 }
+/*
 function initMeshes(){
   // create a geometry
   const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
@@ -77,6 +101,61 @@ function initMeshes(){
   // access the geometry and material at any time using mesh.geometry and mesh.material
   scene.add( mesh );
 }
+*/
+function initMeshes() {
+
+  // create a Group to hold the pieces of the train
+  const train = new THREE.Group();
+  scene.add( train );
+
+  const bodyMaterial = new THREE.MeshStandardMaterial( {
+      color: 0xff3333, // red
+      flatShading: true,
+  } );
+
+  const detailMaterial = new THREE.MeshStandardMaterial( {
+      color: 0x333333, // darkgrey
+      flatShading: true,
+  } );
+
+  const noseGeometry = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 12 );
+  const nose = new THREE.Mesh( noseGeometry, bodyMaterial );
+  nose.rotation.z = Math.PI / 2;
+
+  nose.position.x = -1;
+
+  const cabinGeometry = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
+  const cabin = new THREE.Mesh( cabinGeometry, bodyMaterial );
+  cabin.position.set( 1.5, 0.4, 0 );
+
+  train.add( nose, cabin );
+
+  const wheelGeo = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
+  wheelGeo.rotateX( Math.PI / 2 );
+
+
+  const smallWheelRear = new THREE.Mesh( wheelGeo, detailMaterial );
+  smallWheelRear.position.set( 0, -0.5, 0 );
+
+  const smallWheelCenter = smallWheelRear.clone();
+  smallWheelCenter.position.x = -1;
+
+  const smallWheelFront = smallWheelRear.clone();
+  smallWheelFront.position.x = -2;
+
+  const bigWheel = smallWheelRear.clone();
+  bigWheel.scale.set( 2, 2, 1.25 );
+  bigWheel.position.set( 1.5, -0.1, 0 );
+
+  train.add( smallWheelRear, smallWheelCenter, smallWheelFront, bigWheel );
+
+  const chimneyGeometry = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
+  const chimney = new THREE.Mesh( chimneyGeometry, detailMaterial );
+  chimney.position.set( -2, 0.9, 0 );
+
+  train.add( chimney );
+
+}
 function initRenderer(){
   // create a WebGLRenderer and set its width and height
   renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -100,6 +179,7 @@ function init() {
   scene.background = new THREE.Color( 0x8FBCD4 );
 
   initCamera();
+  initControls();
   initLights();
   initMeshes();
   initRenderer();
@@ -151,9 +231,9 @@ function stop() {
 function update() {
 
   // increase the mesh's rotation each frame
-  mesh.rotation.z += 0.01;
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+  //mesh.rotation.z += 0.01;
+  //mesh.rotation.x += 0.01;
+  //mesh.rotation.y += 0.01;
 
 }
 
